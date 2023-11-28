68ea1bd0f1b4ff783af239dc69939bf23eb2d853
r-silvera <rlucassilvera@gmail.com>
2023-11-28 20:55:23 -0300

    cambios

--- src/components/shared/list-result-card/list-result-card.jsx
+++ src/components/shared/list-result-card/list-result-card.jsx
@@ -17,12 +17,13 @@ export const ListResultCard = ({ list, isLoading }) => {
       {
         (!isLoading)
           ? (
-            <div className='grid lg:grid-cols-4 md:grid-cols-3 sm:grid-cols-2 gap-x-8 gap-y-4'>
+            <div className='grid lg:grid-cols-4 md:grid-cols-3 sm:grid-cols-2 gap-x-12 gap-y-12'>
 
               {
-                list.map( ({id, volumeInfo}) => (
+                list.map( ({id, volumeInfo}, index) => (
                   <SharedSimpleCard 
                     key={id}
+                    index={index}
                     title={volumeInfo.title}
                     authors={volumeInfo}
                     urlImage={(volumeInfo.imageLinks != undefined) ? volumeInfo.imageLinks.thumbnail: ''}
--- src/components/shared/simple-card/shared-simple-card.jsx
+++ src/components/shared/simple-card/shared-simple-card.jsx
@@ -1,28 +1,33 @@
-import { Card, CardBody, CardHeader, Image } from '@nextui-org/react'
+import { Button, Card, CardBody, CardFooter, CardHeader, Image } from '@nextui-org/react'
 import React from 'react'
 import './shared-simple-card.css'
 
-export const SharedSimpleCard = ({title, authors, urlImage}) => {
+export const SharedSimpleCard = ({ index, title, authors, urlImage }) => {
   return (
     <>
-      <Card className="py-3 w-60 simple-card-container">
-        <div className='m-auto'>
-          <CardHeader className="pb-0 pt-2 px-4 flex-col items-start">
-            <p className="text-tiny uppercase font-bold">{title}</p>
-            {/* <small className="text-default-500">{authors}</small> */}
-            {/* <small className="text-default-500">⭐⭐⭐⭐⭐</small> */}
-          </CardHeader>
-          <CardBody className="overflow-visible py-2">
-            <Image
-              alt="Card background"
-              className="object-cover rounded-xl"
-              src={(urlImage)}
-              // width={200}
-            />
-          </CardBody>
 
-        </div>
+
+      <Card
+        isFooterBlurred
+        radius="lg"
+        className="border-none"
+      >
+        <Image
+          alt="Woman listing to music"
+          className="object-cover"
+          height={200}
+          src={urlImage}
+          width={200}
+        />
+        <CardFooter className="justify-between before:bg-white border-white/20 border-1 overflow-hidden py-1 absolute before:rounded-xl rounded-large bottom-1 w-[calc(100%_-_8px)] shadow-small ml-1 z-10">
+          <p className="text-tiny text-black font-bold">{title}</p>
+          <Button className="text-tiny text-white bg-black/20" variant="flat" color="default" radius="lg" size="sm">
+            Notify me
+          </Button>
+        </CardFooter>
       </Card>
+
+
     </>
   )
 }
